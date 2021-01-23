b49c16f538fcabe01af953d8b626894f30356f57
douglas <64352760+Douglas-sp@users.noreply.github.com>
2021-01-23 17:45:45 +0300

    Update client.c

--- client.c
+++ client.c
@@ -13,30 +13,29 @@
 
 int main(int argc, char *argv[]){
 
-//create a socket
 int client_socket;
-client_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-//0 for default protocol, AF_INET for internet socket domain, 
-//sock_stream meaning its a tcp connection socket.
-
 //specify an address for the socket
 struct sockaddr_in serv;
 serv.sin_family = AF_INET;
 serv.sin_port = htons(9002);
 serv.sin_addr.s_addr = INADDR_ANY;
+ //create a socket
+client_socket = socket(AF_INET, SOCK_STREAM, 0);
+//0 for default protocol, AF_INET for internet socket domain, 
+//sock_stream meaning its a tcp connection socket.
 
-int connection_status  = connect(netSocket, (struct sockaddr *)&server_address, sizeof(server_address));
+  inet_pton(AF_INET, "127.0.0.1", &serv.sin_addr);
+  
+int connection_status  = connect(client_socket, (struct sockaddr *)&server_address, sizeof(serv));
 if(connection_status == -1)
 {
 printf("There was a problem with the remote server");
 }
-char server_response[256];
-recv(netSocket, &server_response, sizeof(server_response),0);
+printf("--[+]- COVID-19 CASE MANAGEMENT AND REPORTING TOOL FOR UGANDA -[+]--\n\n");
+  
 
-//print the data we receive from server
-printf("The server sent the data: %s\n", server_response);
-//close socket
+
+ //close socket
 close(netSocket);
 return 0;
 }
